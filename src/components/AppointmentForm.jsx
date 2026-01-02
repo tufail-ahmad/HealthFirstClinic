277054358dfe86d34ecdc860b102b0aaa8da8440
Tufail Ahmad <tufailahmadashrafi@gamil.com>
2026-01-02 23:10:14 +0530

    form updated

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -8,12 +8,73 @@ export default function AppointmentForm({ onClose }) {
           Close
         </button>
         <h3 className={styles.heading}>Book an Appointment</h3>
-        <label htmlFor="patient">Patient Name</label>
+        <label htmlFor="patient" style={{ marginBottom: "-12px" }}>
+          Patient Name:
+        </label>
         <br />
-        <div className={styles["nameinpt-container"]}>
-          <input type="text" id={styles.patient} placeholder="First Name" />
-          <input type="text" id={styles.patient} placeholder="Last Name" />
+        <input type="text" placeholder="First Name" style={{ width: "100%" }} />
+        <br />
+        <input type="text" placeholder="Last Name" style={{ width: "100%" }} />
+        <br />
+        <label>Gender:</label>
+        <div className={styles["gender-container"]}>
+          <label htmlFor="male">Male</label>
+          <input
+            type="radio"
+            value="male"
+            name="gender"
+            id="male"
+            style={{ marginLeft: "5px" }}
+          />
+          <label htmlFor="female" style={{ marginLeft: "5px" }}>
+            Female
+          </label>
+          <input
+            type="radio"
+            value="female"
+            name="gender"
+            id="female"
+            style={{ marginLeft: "5px" }}
+          />
         </div>
+        <label htmlFor="age">Age:</label>
+        <br />
+        <input
+          type="number"
+          id="age"
+          placeholder="00"
+          style={{ width: "100%" }}
+        />{" "}
+        <br />
+        <label htmlFor="city">City:</label>
+        <input
+          type="text"
+          id="city"
+          style={{ width: "100%" }}
+          placeholder="City"
+        />
+        <label htmlFor="mobile">Mobile No:</label>
+        <br />
+        <input
+          type="number"
+          name="mobile no."
+          id="mobile"
+          placeholder="+91 0000000000"
+          style={{ width: "100%" }}
+        />
+        <br />
+        <label htmlFor="text">Message:</label>
+        <br />
+        <textarea
+          name="message"
+          id="text"
+          spellCheck="true"
+          wrap="soft"
+          rows={6}
+          cols={60}
+          placeholder="Enter your message"
+          className={styles.textarea}
+        ></textarea>
       </form>
     </div>
   );
